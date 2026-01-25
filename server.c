b6d858cf133ce9b53f8d96dbce341af243f0af39
kos6490 <kos-6490@naver.com>
2026-01-25 21:06:25 +0900

    feat: Serve index.html to browser

--- server.c
+++ server.c
@@ -13,6 +13,7 @@ int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
+    int opt = 1;
     
     // 1. 소켓 생성
     // AF_INET: IPv4, SOCK_STREAM: TCP
@@ -25,6 +26,11 @@ int main() {
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY; // 내 컴퓨터의 모든 IP로 접속 허용
     address.sin_port = htons(PORT);       // 포트 번호 8080
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
 
     // 3. 바인딩
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
@@ -40,28 +46,44 @@ int main() {
 
     printf("Server is listening on port %d...\n", PORT);
 
-    // 5. 수락
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
+    // 5. 수락 & 데이터 주고받기
+    while(1) {
+        // 연결 수락
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        char buffer[BUFFER_SIZE] = {0};
+        read(new_socket, buffer, BUFFER_SIZE);
+        printf("Request: \n%s\n", buffer);
+
+        // index.html 파일 열기
+        FILE *file = fopen("index.html", "r");
+        
+        if (file == NULL) {
+            printf("Error: File not found\n");
+            char *err_msg = "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found";
+            send(new_socket, err_msg, strlen(err_msg), 0);
+        } else {
+            // 헤더 전송
+            char *header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+            send(new_socket, header, strlen(header), 0);
+
+            // 파일 내용 전송
+            char file_buf[BUFFER_SIZE];
+
+            while (fgets(file_buf, BUFFER_SIZE, file) != NULL) {
+                send(new_socket, file_buf, strlen(file_buf), 0);
+            }
+            
+            fclose(file);
+            printf(">> File sent successfully!\n");
+        }
+
+        close(new_socket);
     }
     
-    printf("Client connected!\n");
-
-    // 6. 데이터 주고받기
-    while(1) {
-        memset(buffer, 0, BUFFER_SIZE); // 버퍼 비우기
-        int valread = read(new_socket, buffer, BUFFER_SIZE);
-        if (valread <= 0) break; // 연결 끊기면 종료
-
-        printf("Client said: %s\n", buffer);
-        
-        // 그대로 돌려주기 (Echo)
-        send(new_socket, buffer, strlen(buffer), 0);
-    }
-
-    printf("Client disconnected.\n");
-    close(new_socket);
     close(server_fd);
     return 0;
 }
